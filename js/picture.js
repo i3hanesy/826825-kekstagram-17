e3a73467185f01da8dc4bc1891ee15cfeee31df7
Andrey <i3hanesy@yandex.ru>
2019-07-17 15:31:45 +0500

    функция взаимодействия с удаленным сервером через XHR

--- js/picture.js
+++ js/picture.js
@@ -1,29 +1,41 @@
 'use strict';
 (function () {
-// создает элементы из массива объектов по шаблону
-  var pictures = document.querySelector('.pictures');
-  var pictureTemplate = document.querySelector('#picture').content.querySelector('a');
-  var fragment = document.createDocumentFragment();
 
-  var getTemplatesElements = function (templateElement, objectsArray) {
-    for (var i = 0; i < objectsArray.length; i++) {
-      var element = templateElement.cloneNode(true);
-      var object = objectsArray[i];
-      element.querySelector('.picture__comments').textContent = object.message;
-      element.querySelector('.picture__likes').textContent = object.likes;
-      element.querySelector('.picture__img').setAttribute('src', object.url);
-      fragment.appendChild(element);
-    }
+  // var onError = function (message) {
+  //   console.error(message);
+  // };
 
-    return element;
+  var onSuccess = function (data) {
+
+    //  создает элементы из массива объектов по шаблону
+    var pictures = document.querySelector('.pictures');
+    var pictureTemplate = document.querySelector('#picture').content.querySelector('a');
+    var fragment = document.createDocumentFragment();
+
+    var getTemplatesElements = function (templateElement, objectsArray) {
+      for (var i = 0; i < objectsArray.length; i++) {
+        var element = templateElement.cloneNode(true);
+        var object = objectsArray[i];
+        element.querySelector('.picture__comments').textContent = object.comments;
+        element.querySelector('.picture__likes').textContent = object.likes;
+        element.querySelector('.picture__img').setAttribute('src', object.url);
+        fragment.appendChild(element);
+      }
+
+      return element;
+    };
+
+    getTemplatesElements(pictureTemplate, data);
+
+    // вставляет элементы из шаблона на страницу
+    var insertElements = function (locationOfInsertion) {
+      locationOfInsertion.appendChild(fragment);
+    };
+
+    insertElements(pictures);
   };
 
-  getTemplatesElements(pictureTemplate, window.data.photos);
+  window.load('https://js.dump.academy/kekstagram/data', onSuccess);
 
-  // вставляет элементы из шаблона на страницу
-  var insertElements = function (locationOfInsertion) {
-    locationOfInsertion.appendChild(fragment);
-  };
-
-  insertElements(pictures);
 })();
+
