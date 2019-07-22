402dd3f1301c792ab44af076399ea0885b077e85
Andrey <i3hanesy@yandex.ru>
2019-07-23 00:58:58 +0500

    перламутровые пуговицы этап первый

--- js/picture.js
+++ js/picture.js
@@ -3,10 +3,12 @@
 
   var XHR_URL = 'https://js.dump.academy/kekstagram/data';
 
-  var onSuccess = function (data) {
+  var imgFilter = document.querySelector('.img-filters');
+  var imgFiltersForm = imgFilter.querySelector('.img-filters__form');
+  var pictures = document.querySelector('.pictures');
 
-    //  создает элементы из массива объектов по шаблону
-    var pictures = document.querySelector('.pictures');
+
+  var getPictures = function (dataArray) {
     var pictureTemplate = document.querySelector('#picture').content.querySelector('a');
     var fragment = document.createDocumentFragment();
 
@@ -23,7 +25,8 @@
       return element;
     };
 
-    getTemplatesElements(pictureTemplate, data);
+    getTemplatesElements(pictureTemplate, dataArray);
+
 
     // вставляет элементы из шаблона на страницу
     var insertElements = function (locationOfInsertion) {
@@ -33,7 +36,59 @@
     insertElements(pictures);
   };
 
+  var onSuccess = function (data) {
+
+    var dataCopy = data.slice();
+
+    getPictures(data);
+    imgFilter.classList.remove('img-filters--inactive');
+
+    imgFiltersForm.addEventListener('click', function (evt) {
+      var target = evt.target;
+      var typeButton = target.type === 'button';
+      if (typeButton && target.id === 'filter-popular') {
+
+        var picture = pictures.querySelectorAll('.picture');
+        picture.parentNode.removeChild(picture);
+
+        getPictures(data);
+
+      } else if (typeButton && target.id === 'filter-new') {
+
+        var shuffleArray = function (arr) {
+          var j;
+          var temp;
+          for (var i = arr.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+          }
+
+          return arr;
+        };
+
+        shuffleArray(dataCopy);
+        getPictures(dataCopy.slice(0, 10));
+
+      } else if (typeButton && target.id === 'filter-discussed') {
+
+        dataCopy.sort(function (first, second) {
+          if (first.comments.length < second.comments.length) {
+            return 1;
+          } else if (first.comments.length > second.comments.length) {
+            return -1;
+          } else {
+            return 0;
+          }
+        });
+        getPictures(dataCopy);
+
+      }
+    });
+
+  };
+
   window.load(XHR_URL, onSuccess);
 
 })();
-
